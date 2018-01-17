02600be28cd9f2070142cd50120b0f0e1d8fe011
Daniel Cousens <github@dcousens.com>
2018-01-17 11:50:50 +1100

    default to SHA1 for native

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var crypto = require('crypto')
+var native = require('crypto')
+
+function nativePBKDF2 (password, salt, iterations, keylen, digest, callback) {
+  return native.pbkdf2(password, salt, iterations, keylen, digest || 'sha1', callback)
+}
+
+function nativePBKDF2Sync (password, salt, iterations, keylen, digest) {
+  return native.pbkdf2Sync(password, salt, iterations, keylen, digest || 'sha1')
+}
+
 /* istanbul ignore next */
-if (crypto && (!crypto.pbkdf2Sync || crypto.pbkdf2Sync.toString().indexOf('keylen, digest') === -1)) {
-  exports.pbkdf2 = require('./lib/async')
-  exports.pbkdf2Sync = require('./lib/sync')
+if (!native.pbkdf2Sync || native.pbkdf2Sync.toString().indexOf('keylen, digest') === -1) {
+  module.exports.pbkdf2Sync = require('./lib/sync')
+  module.exports.pbkdf2 = require('./lib/async')
+
+// native
 } else {
-  exports.pbkdf2Sync = crypto.pbkdf2Sync
-  exports.pbkdf2 = crypto.pbkdf2
+  module.exports.pbkdf2Sync = nativePBKDF2Sync
+  module.exports.pbkdf2 = nativePBKDF2
 }
